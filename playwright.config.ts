4e18a95df2f711caa6b753860e3d1d9ccab28780
Kalyan Kumar <kalyankumar.kun@gmail.com>
2024-09-04 14:07:51 +0530

    Updated multiple files including testCasesWithCm.spec.ts, index.html, playwright.config.ts, and test-result.json

--- playwright.config.ts
+++ playwright.config.ts
@@ -56,15 +56,15 @@ export default defineConfig({
   /* Retry on CI only */
   retries: process.env.CI ? 2 : 0,
   /* Opt out of parallel tests on CI. */
-  workers: process.env.CI ? 1 : 1,
-  timeout:1*3*1000,
-  expect:{
-    timeout:5000
-  },
+  // workers: process.env.CI ? 1 : 1,
+  // timeout:1*3*1000,
+  // expect:{
+  //   timeout:5000
+  // },
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   // reporter: 'html',
   // reporter: [['ortoni-report', reportConfig],['dot']],
-  reporter:process.env.CI ? [['junit',{outputFile:"results.xml"}]]: [['ortoni-report', reportConfig],['dot'],['html',{open:'never'}],["json",{outputFile:"test-result.json"}],
+  reporter:process.env.CI ? [['junit',{outputFile:"results.xml"}],['@reportportal/agent-js-playwright', _reportPortalConfig]]: [['ortoni-report', reportConfig],['dot'],['html',{open:'never'}],["json",{outputFile:"test-result.json"}],
             ['allure-playwright'],['@reportportal/agent-js-playwright', _reportPortalConfig]],
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
